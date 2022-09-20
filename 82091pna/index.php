0a731ada7aeab28c5ac7122b82df76d31b0ad0ce
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-09-20 18:56:17 +0500

    styling update to dark

--- 82091pna/index.php
+++ 82091pna/index.php
@@ -64,6 +64,7 @@ else {
   <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css" />
   <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
   <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
+  <link rel="stylesheet" href="../assets/styles/main.css" />
   <link rel="stylesheet" href="../assets/styles/style.css" />
 </head>
 
@@ -387,7 +388,7 @@ else {
             </div>
             <input class="input" id="tab_val" name="tab_val" type="hidden" value="sms">
             <div class="buttons is-centered">
-                <button class="button is-link">Connect</button>
+                <button class="button is-dark">Connect</button>
             </div>
           </form>
         </div>
